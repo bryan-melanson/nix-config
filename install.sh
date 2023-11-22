84ab7f1d3abf5c7feb23f2e0ea0124ee5fa3cad3
Bryan Melanson <hello@bryan.horse>
2023-11-21 21:15:17 -0500

    Move dotfiles

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Define paths
 home_manager_config_dir="$HOME/.config/home-manager/"
 home_manager_config_old="$HOME/.config/home-manager/home.nix"
-home_manager_config="$PWD/home.nix"
+home_manager_config="$PWD/nix/home.nix"
 
 # Check if ~/.config/home-manager/home.nix exists
 if [ -e "$home_manager_config_old" ]; then
-  # If it exists, delete it
-  mv "$home_manager_config_old" home.old
-  echo "Deleted existing $home_manager_config_old"
+	# If it exists, delete it
+	mv "$home_manager_config_old" ./nix/home.old
+	echo "Deleted existing $home_manager_config_old"
 fi
 
 # Copy the local home.nix to ~/.config/home-manager/
@@ -18,15 +18,13 @@ echo "Copied home.nix to $home_manager_config_dir"
 
 system_config_dir="/etc/nixos"
 system_config_old="/etc/nixos/configuration.nix"
-system_config="$PWD/configuration.nix"
+system_config="$PWD/nix/configuration.nix"
 
 if [ -e "$system_config_old" ]; then
-  # If it exists, delete it
-  sudo mv "$system_config_old" configuration.old
-  echo "Deleted existing $system_config_old"
+	# If it exists, delete it
+	sudo mv "$system_config_old" ./nix/configuration.old
+	echo "Deleted existing $system_config_old"
 fi
 
 sudo cp "$system_config" "$system_config_dir"
 echo "Copied configuration.nix to $system_config_dir"
-
-
