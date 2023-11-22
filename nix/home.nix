b1b49cb00f40677ca96a1a31c87c873f33cef8da
Bryan Melanson <hello@bryan.horse>
2023-11-21 21:47:53 -0500

    Update home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -57,11 +57,11 @@ in
 	programs.kitty = {
 		enable = true;
 		extraConfig = {
-			font_family      JetBrains Mono Medium
-			bold_font        JetBrains Mono Bold
-			italic_font      JetBrains Mono Italic
-			bold_italic_font JetBrains Mono Bold Italic
-			font_size 14.0	
+			font_family = JetBrains Mono Medium;
+			bold_font = JetBrains Mono Bold;
+			italic_font = JetBrains Mono Italic;
+			bold_italic_font = JetBrains Mono Bold Italic;
+			font_size = 14.0;
 		};
 		theme = "Dracula";
 	};
