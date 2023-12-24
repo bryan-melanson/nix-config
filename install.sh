c20ecfbe2d06e3570ae105e06bcf97eb6104cf68
Bryan Melanson <hello@bryan.horse>
2023-12-23 20:08:32 -0500

    Add script

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt install git curl
+sudo snap install nvim
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd /usr/local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFontMono-Regular.otf
